ca494e011e28c19274e54f4c01e33d44e25bb093
Ayush12345-Gupta <ayushgupta5300@gmail.com>
2026-02-18 12:04:18 +0530

    First commit

---
+++ kokoeatinbanana.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=high;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            long long hours=0;
+            for(int i=0;i<piles.size();i++){
+                if(piles[i]%mid!=0){
+                    hours++;
+                }
+                hours+=piles[i]/mid;
+            }
+            if(hours<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
+        }
+        return ans;
+        
+    }
+};
